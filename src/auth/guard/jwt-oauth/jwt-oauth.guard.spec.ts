68fd31715e2c69a3eac29c096fb9b55720a330db
horusen <kane.babacar@protonmail.com>
2023-09-16 06:31:28 +0000

    Fix: Unit test for JwtOauthGuard

--- src/auth/guard/jwt-oauth/jwt-oauth.guard.spec.ts
+++ src/auth/guard/jwt-oauth/jwt-oauth.guard.spec.ts
@@ -1,7 +1,16 @@
+import { createMock } from '@golevelup/ts-jest';
 import { JwtOauthGuard } from './jwt-oauth.guard';
+import { ConfigService } from '@nestjs/config';
+import { SessionService } from '../../../session/session.service';
+import { Reflector } from '@nestjs/core';
 
 describe('JwtOauthGuard', () => {
-  // it('should be defined', () => {
-  //   expect(new JwtOauthGuard()).toBeDefined();
-  // });
+  const mockConfigService = createMock<ConfigService>();
+  const mockSessionService = createMock<SessionService>();
+  const mockReflector = createMock<Reflector>();
+  it('should be defined', () => {
+    expect(
+      new JwtOauthGuard(mockConfigService, mockSessionService, mockReflector),
+    ).toBeDefined();
+  });
 });
